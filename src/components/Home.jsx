1c995c3e6d37787d8849bbfbb8ee6e0a2232772c
Rajan <chaudharyrajan903@gmail.com>
2022-11-28 16:43:51 +0545

    add expression

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,32 @@
+// import React, { useState } from 'react'
+
+// const Home = () => {
+//   const [count,setCount]=useState(0);
+//   return (<>
+//       <h1>{count}</h1>
+//       <button onClick={()=>setCount(count+1)}>Increment</button>
+//       <button onClick={()=>setCount(count-1)}>Decrement</button>
+//     </>
+//   )
+// }
+
+// export default Home
+
+
 import React from 'react'
+import abc from "../images/abc.jpg";
+import "./home.css"
 
 const Home = () => {
   return (
-      <div>home</div>
+    <>
+      <h1 className='header'>abc ever</h1>
+      <div className='img_div'>
+      <img src= {abc} alt="wakanda" />
+      <img src= {abc} alt="wakanda" />
+      <img src= {abc} alt="wakanda" />
+      </div>
+    </>
   )
 }
 
