928c3769bfa8078815b714c2106a8d6921848d1b
Rajan <chaudharyrajan903@gmail.com>
2022-12-14 15:35:07 +0545

    return function

--- src/components/pages/user/Register.jsx
+++ src/components/pages/user/Register.jsx
@@ -1,57 +1,88 @@
-import React, { useRef, useState } from "react";
-//import Login from './Login';
+// import React, { useRef, useState } from "react";
+// //import Login from './Login';
+
+// // const Register = () => {
+// //   const [count, setCount] = useState(0);
+// //   const [data, setData] = useState(10);
+
+// //   // const myMemo = useMemo(() => {
+// //   //   return count * 5;
+// //   // }, [count]);
+
+// //   // const mmyMemo = useMemo(() => {
+// //   //   return count / 2;
+// //   // }, [count]);
+// //   // return (
+// //     <>
+// //       <div>
+// //         {/* <Login count={count}/> */}
+// //         <h1>Count:{count}</h1>
+// //         <h1>Data:{data}</h1>
+// //         <p>{myMemo}</p>
+// //         <p>{mmyMemo}</p>
+// //         <button onClick={() => setCount(count + 1)}>Update Count</button>
+// //         <button onClick={() => setData(data * 5)}>Update Data</button>
+// //       </div>
+// //     </>
+// //   );
+// // };
+
 
 // const Register = () => {
-//   const [count, setCount] = useState(0);
-//   const [data, setData] = useState(10);
+//   const [show,setShow]=useState(false);
+//   const inputRef= useRef(null);
 
-//   // const myMemo = useMemo(() => {
-//   //   return count * 5;
-//   // }, [count]);
+//   const handleClick=(event)=>{
+//   event.preventDefault();
 
-//   // const mmyMemo = useMemo(() => {
-//   //   return count / 2;
-//   // }, [count]);
-//   // return (
+//   const name=inputRef.current.value;
+//   name === ""? alert("please fill the form"): setShow(true);
+//   };
+//   return (
 //     <>
-//       <div>
-//         {/* <Login count={count}/> */}
-//         <h1>Count:{count}</h1>
-//         <h1>Data:{data}</h1>
-//         <p>{myMemo}</p>
-//         <p>{mmyMemo}</p>
-//         <button onClick={() => setCount(count + 1)}>Update Count</button>
-//         <button onClick={() => setData(data * 5)}>Update Data</button>
-//       </div>
+//     <div>
+//       <label>Enter your data</label>
+//       <br/>
+//       <input type="text" ref={inputRef}/>
+//       <br/>
+//       <button onClick={handleClick}>Click Me</button>
+//       <br/>
+//       <p>{show? `your lucky number is :${inputRef.current.value}`:" "}</p>
+//     </div>
+    
 //     </>
-//   );
-// };
+//   )
+// }
 
+// export default Register;
+
+import React, { useEffect, useState } from 'react';
 
 const Register = () => {
-  const [show,setShow]=useState(false);
-  const inputRef= useRef(null);
+  const [count,setCount]=useState(0);
 
-  const handleClick=(event)=>{
-  event.preventDefault();
+  useEffect(()=>{
+    console.log("useEffect Runs")
+    const interval=setInterval(() => {
+     setCount((prev)=>prev+1); 
+    }, 1000);
 
-  const name=inputRef.current.value;
-  name === ""? alert("please fill the form"): setShow(true);
-  };
-  return (
+    return ()=>{
+      clearInterval(interval);
+      console.log("cleaning the effect");
+    }
+ },[]);
+
+return(
+  
+
     <>
     <div>
-      <label>Enter your data</label>
-      <br/>
-      <input type="text" ref={inputRef}/>
-      <br/>
-      <button onClick={handleClick}>Click Me</button>
-      <br/>
-      <p>{show? `your lucky number is :${inputRef.current.value}`:" "}</p>
+      <h1>Count:{count}cleaning return function </h1>
     </div>
     
     </>
   )
 }
 
-export default Register;
+export default Register
